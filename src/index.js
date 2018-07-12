4f2f883f00a1cd5ee716f4a0d9b56151a383d2b9
Will Floyd <willyfresh@gmail.com>
2018-07-11 22:38:18 -0700

    structured as IIFE; click listener for generate link button (instead of being called in button's html element attribute); changed my commenting style and improved legibility; new minified JS file

--- src/index.js
+++ src/index.js
@@ -1,168 +1,203 @@
-/*!
+/*
  *
  * Tracking Link Generator for UTM/ITM
  * 2018 Will Floyd (http://willfloyd.com)
  * Mozilla Public License
- * Version: 0.0.2
+ * Version: 0.0.3
  *
  */
 
-/*
-================================
-initialization
-to make the page ready to go
-================================
-*/
-const initGenerator = function(){
+(function campaignLinkGenerator() { // IIFE aka "iffy"
+
 	/*
-	==========================================
-	are domain restrictions in place?
-	add domain to beginning of the url input
-	==========================================
+	===============
+	INITIALIZATION
+	===============
 	*/
-	if (settings.domain) $('#url-param-entry').before('<div class="input-group-prepend"><span class="input-group-text" id="utm-base-url">'+settings.domain+'</span></div>');
-	/* 
-	========================
-	campaigns auto-complete
-	coming soon
-	tutorial saved***
-	========================
-	*/
-	/*
-	====================================================
-	source/medium arrays generated: internal & external
-	two arrays are easier to iterate in next step
-	====================================================
-	*/
-	for (const medium in settings.sources.external) {
-	settings.sources.external[medium].forEach(function(source) {
-		extSourceTable.push([medium, source]);
-	});
-}
-	// ===========================================================
-	for(const medium in settings.sources.internal){
-		settings.sources.internal[medium].forEach(function(source){
-			intSourceTable.push([medium,source]);
+
+	// source selection table variables
+
+	clgSettings.sources.tables = {};
+	clgSettings.sources.tables.external = [];
+	clgSettings.sources.tables.internal = [];
+	clgSettings.sources.tables.html = '';
+
+	// link building variable
+
+	clgSettings.link = {};
+
+
+	// optional domain restriction, remove from settings to turn off
+	// adds base url to input field
+
+	if (clgSettings.domain) $('#url-param-entry').before('<div class="input-group-prepend"><span class="input-group-text" id="utm-base-url">' + clgSettings.domain + '</span></div>');
+
+	// activate autocomplete for campaign names input
+	// COMING SOON
+
+	// source/medium arrays populated
+	// two arrays are easier to iterate in next step
+
+	for (const medium in clgSettings.sources.external) {
+		clgSettings.sources.external[medium].forEach(function(source) {
+			clgSettings.sources.tables.external.push([medium, source]);
 		});
 	}
+
+	for (const medium in clgSettings.sources.internal) {
+		clgSettings.sources.internal[medium].forEach(function(source) {
+			clgSettings.sources.tables.internal.push([medium, source]);
+		});
+	}
+
+	// populate sources table:
+
+	{ // block scope to contain helper variables
+
+		let rowCount = 0;
+		let foreman = 0;
+
+		// do > while loop to ensure the table at least gets one row
+
+		do {
+			clgSettings.sources.tables.html += '<tr><td>'
+
+			// add external option to first column
+
+			if (clgSettings.sources.tables.external[rowCount]) {
+				let extLowerCase = clgSettings.sources.tables.external[rowCount][1].toString().toLowerCase();
+				clgSettings.sources.tables.html += '<div class="form-check"><input class="form-check-input" type="radio" name="source-param-entry" id="source-' + extLowerCase + '" value="' + extLowerCase + '" data-scope="external" data-medium="' + clgSettings.sources.tables.external[rowCount][0] + '"><label class="form-check-label" for="source-' + extLowerCase + '">' + clgSettings.sources.tables.external[rowCount][1] + '</label></div>'
+			}
+
+			// elseif to display an error for an empty table
+			// coming soon
+
+			clgSettings.sources.tables.html += '</td><td>';
+
+			// add internal option to second column
+
+			if (clgSettings.sources.tables.internal[rowCount]) {
+				let intLowerCase = clgSettings.sources.tables.internal[rowCount][1].toString().toLowerCase();
+				clgSettings.sources.tables.html += '<div class="form-check"><input class="form-check-input" type="radio" name="source-param-entry" id="source-' + intLowerCase + '" value="' + intLowerCase + '" data-scope="internal" data-medium="' + clgSettings.sources.tables.internal[rowCount][0] + '"><label class="form-check-label" for="source-' + intLowerCase + '">' + clgSettings.sources.tables.internal[rowCount][1] + '</label></div>'
+			}
+
+			clgSettings.sources.tables.html += '</td></tr>';
+
+			// helper variables
+			// foreman checks next row and ends loop if there's no more sources
+
+			rowCount++;
+			foreman = 0;
+			if (!clgSettings.sources.tables.external[rowCount]) foreman++;
+			if (!clgSettings.sources.tables.internal[rowCount]) foreman++;
+		}
+		while (foreman < 2);
+
+		// table inserted into DOM
+
+		$('#source-param tbody').html(clgSettings.sources.tables.html);
+
+	}
+
+	// content and term are optional
+	// to remove: add 'no' to settings object
+
+	if (clgSettings.content === 'no') $('#content-param').remove();
+	if (clgSettings.term === 'no') $('#term-param').remove();
+
 	/*
-	========================================
-	iterating through the sourceTable arrays
-	building the html table row by row
-	foreman variable watches do/while loop
-	foreman ends loop at end of both arrays
-	printing into the table body
-	========================================
+	===============
+	LINK GENERATOR
+	===============
 	*/
-	let sourceTableHTML = '';
-	let rowCount=0;
-	let foreman=0;
 
-	do {
-		sourceTableHTML += '<tr><td>'
+	// watches for click on link-generator button
 
-		if (extSourceTable[rowCount]) {
-			let extLowerCase=extSourceTable[rowCount][1].toString().toLowerCase();
-			sourceTableHTML += '<div class="form-check"><input class="form-check-input" type="radio" name="source-param-entry" id="source-'+extLowerCase+'" value="'+extLowerCase+'" data-scope="external" data-medium="'+extSourceTable[rowCount][0]+'"><label class="form-check-label" for="source-'+extLowerCase+'">'+extSourceTable[rowCount][1]+'</label></div>'
+	$('#link-generator').click(function() {
+
+		// remove copyButton from DOM
+
+		if ($('#copyButton')) $('#copyButton').remove();
+
+		// capture url
+		// url validation coming soon - limit characters
+
+		clgSettings.link.url = clgSettings.domain + document.getElementById('url-param-entry').value.toLowerCase();
+
+		// capture campaign name
+		// 1st: validate that name exists
+
+		clgSettings.link.campaign = document.getElementById('campaign-param-entry').value;
+
+		// add source, medium and scope to link
+
+		const fullSourceTable = clgSettings.sources.tables.external.concat(clgSettings.sources.tables.internal);
+
+		// find out which radio button is checked
+		// action for if nothing is checked coming soon
+
+		fullSourceTable.forEach(function(source) {
+
+			let sourceID = 'source-' + source[1].toLowerCase();
+
+			if (document.getElementById(sourceID).checked) {
+
+				clgSettings.link.source = document.getElementById(sourceID).value;
+				clgSettings.link.medium = document.getElementById(sourceID).dataset.medium;
+				clgSettings.link.scope = document.getElementById(sourceID).dataset.scope;
+
+			}
+
+		});
+
+		// optional content field
+		// validation and character limitation coming soon
+
+		if (clgSettings.content != 'no') {
+			clgSettings.link.content = document.getElementById('content-param-entry').value;
 		}
 
-		sourceTableHTML += '</td><td>';
+		// optional term field
+		// still hasn't been used yet
 
-		if(intSourceTable[rowCount]){
-			let intLowerCase=intSourceTable[rowCount][1].toString().toLowerCase();
-			sourceTableHTML += '<div class="form-check"><input class="form-check-input" type="radio" name="source-param-entry" id="source-'+intLowerCase+'" value="'+intLowerCase+'" data-scope="internal" data-medium="'+intSourceTable[rowCount][0]+'"><label class="form-check-label" for="source-'+intLowerCase+'">'+intSourceTable[rowCount][1]+'</label></div>'
+		if (clgSettings.term != 'no') {
+			clgSettings.link.term = document.getElementById('term-param-entry').value;
 		}
 
-		sourceTableHTML += '</td></tr>';
+		// switch decides which url parameters to use
+		// link is generated with appropriate parameters
 
-		foreman=0;
-		if(!extSourceTable[rowCount+1]) foreman++;
-		if(!intSourceTable[rowCount+1]) foreman++;
-		rowCount++;
-	}
-	while(foreman<2);
+		switch (clgSettings.link.scope) {
 
-	$('#source-param tbody').html(sourceTableHTML);
-	/*
-	=======================================
-	content and term are totalllinktional
-	to remove: add 'no' to settings object
-	=======================================
-	*/
-	if (settings.content === 'no') $('#content-param').remove();
-	if (settings.term === 'no') $('#term-param').remove();
-}
+			case 'external':
 
-/*
-=========================================
-link generator
-validates the entries (added soon)
-builds link based on form values
-reveals the copy button
-==========================================
-*/
-const generateLink = function(){
-if ($('#copyButton')) $('#copyButton').remove(); // remove copyButton so there isn't two.
+				clgSettings.link.full = clgSettings.link.url + '?utm_campaign=' + clgSettings.link.campaign + '&utm_source=' + clgSettings.link.source + '&utm_medium=' + clgSettings.link.medium;
+				if (clgSettings.content != 'no') clgSettings.link.full += '&utm_content=' + clgSettings.link.content;
+				if (clgSettings.term != 'no') clgSettings.link.full += '&utm_term=' + clgSettings.link.term;
+				break;
 
-const linkURL = settings.domain + document.getElementById('url-param-entry').value.toLowerCase(); // mitigate url and backslash doubleups
-const linkCampaign = document.getElementById('campaign-param-entry').value; // spaces to dashes, limit to dashes and alphanumeric
-/* 
-=========================================================
-okay, using radio buttons isn't as easy as I had hoped.
-if i want values from them, i have to refer to them by ID
-example: #source-streamsend
-then I can grab dataset and value from the input field
+			case 'internal':
 
-but first i need to identify which one is checked...
-this means I need to test each one and return  
-=========================================================
-*/
+				clgSettings.link.full = clgSettings.link.url + '?itm_campaign=' + clgSettings.link.campaign + '&itm_source=' + clgSettings.link.source + '&itm_medium=' + clgSettings.link.medium;
+				if (clgSettings.content != 'no') clgSettings.link.full += '&itm_content=' + clgSettings.link.content;
+				if (clgSettings.term != 'no') clgSettings.link.full += '&itm_term=' + clgSettings.link.term;
+				break;
 
-// gotta scope these variables properly
-let linkSource = '';
-let linkMedium = '';
-let linkScope = '';
-let linkContent = '';
-let linkTerm = '';
+			default:
 
-const fullSourceTable = extSourceTable.concat(intSourceTable);
-fullSourceTable.forEach(function(source){
-	let sourceID = 'source-'+source[1].toLowerCase();
-	if(document.getElementById(sourceID).checked){
-		linkSource = document.getElementById(sourceID).value;
-		linkMedium = document.getElementById(sourceID).dataset.medium;
-		linkScope = document.getElementById(sourceID).dataset.scope;
-	}
-});
-if (settings.content != 'no') {
-	linkContent = document.getElementById('content-param-entry').value; // spaces to dashes, limit to dashes and alphanumeric
-}
-if (settings.term != 'no'){
-	linkTerm = document.getElementById('term-param-entry').value; // if I knew more about terms, I would probably develop something for it's text box.
-}
+				clgSettings.link.full = "ERROR";
 
-let campaignLink = '';
-switch(linkScope){
-	case 'external':
-		campaignLink = linkURL+'?utm_campaign='+linkCampaign+'&utm_source='+linkSource+'&utm_medium='+linkMedium;
-		if (settings.content != 'no') campaignLink += '&utm_content='+linkContent;
-		if (settings.term != 'no') campaignLink += '&utm_term='+linkTerm;
-		break;
-	case 'internal':
-		campaignLink = linkURL+'?itm_campaign='+linkCampaign+'&itm_source='+linkSource+'&itm_medium='+linkMedium;
-		if (settings.content != 'no') campaignLink += '&itm_content='+linkContent;
-		if (settings.term != 'no') campaignLink += '&itm_term='+linkTerm;
-		break;
-	default:
-		campaignLink = "ERROR";
-}
-$('#gen-result').html(campaignLink);
+		}
 
-/*
-================
-add copy button
-================
-*/
-$('#gen-buttons').append(' <button id="copyButton" class="btn btn-outline-primary" data-clipboard-text="'+campaignLink+'">Copy to Clipboard</button>');
-new ClipboardJS('#copyButton');
-}
+		$('#gen-result').html(clgSettings.link.full);
+
+		// add copyButton to DOM
+
+		$('#gen-buttons').append(' <button id="copyButton" class="btn btn-outline-primary" data-clipboard-text="' + clgSettings.link.full + '">Copy to Clipboard</button>');
+		new ClipboardJS('#copyButton');
+
+		console.log(clgSettings); // show me the object!
+
+	});
+
+}());
