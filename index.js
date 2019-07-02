d1f1669af89b5b2599d3ba2fc87fe16112407a94
Will Floyd <willyfresh@gmail.com>
2019-07-01 20:40:19 -0700

    simplified interface - one link at a time
    changes:
    - changed checkboxes to select menu - source/medium
    - made adjustments in object to accommodate simplification
    - put repeated code into a function - validChars()
    fixes:
    - put code back inside closure

--- index.js
+++ index.js
@@ -1,104 +1,97 @@
-================
- INITIALIZATION
-================
-*/
+/*
+ *
+ * Campaign Link Generator for UTM/ITM
+ * Mozilla Public License
+ * Version: 0.0.5
+ *
+ */
 
+( function( $ ) {
+    // entry and results object
+    const ltObj = {
+        'url': '',
+        'campaign': '',
+        'term': '',
+        'source': '',
+        'medium': '',
+        'lcontent': '',
+        'trackingLink': '',
+        'htmlOutput': ''
+    };
 
-ltObj = {
-    'url': '',
-    'campaign': '',
-    'term': '',
-    'd_attribute': [],
-    'htmlOutput': [],
-    'htmlCopy': ''
-};
-//force HTTPS compliance
-function checkProtocol(iURL) {
-    var pURL;
-    if (!(iURL.search(/[^:\/\/]*$/) == 0)) {
-        pURL = iURL.match(/[^:\/\/]*$/);
-        pURL = 'https://' + pURL;
+    // watch for changes in source select
+    $( '#source-param-entry' ).change( function() {
 
-    } else if (!(iURL.search(/[^:\/\/]*$/) == 1)) {
-        pURL = iURL;
-        pURL = 'https://' + pURL;
-    }
-    return pURL;
-}
+        // collect data attributes
+        ltObj.source = $( 'option:selected',this ).attr( 'data-source' );
+        ltObj.medium = $( 'option:selected',this ).attr( 'data-medium' );
 
-function GenerateLinks() {
+        // collect and validate text entries
+        ltObj.url = $( '#url-param-entry' ).val().toLowerCase();
+        ltObj.url = checkProtocol(ltObj.url);
+        ltObj.campaign = validChars( $( '#campaign-param-entry' ) );
+        ltObj.lcontent = validChars( $( '#content-param-entry' ) );
+        ltObj.term = validChars( $( '#term-param-entry' ) );
 
-    ltObj.d_attribute = [];
-    ltObj.htmlOutput = [];
-    ltObj.htmlCopy = '';
-//Checkbox Listener - Push HTML Custom attr data in
-    $("#link-param").find("input[type=checkbox]").each(function() {
+        // generate links
+        // @TODO: add error handling for empty required fields
 
-        if ($(this).is(":checked")) {
-            var d_source = $(this).attr('data-source');
-            $(this).attr('data-source');
-            var d_medium = $(this).attr('data-medium');
-            ltObj.d_attribute.push({
-                "d_source": d_source,
-                "d_medium": d_medium
-            });
-        }
-    });
-//text validation for each entry
-    ltObj.url = $('#url-param-entry').val().toLowerCase();
-    ltObj.url = checkProtocol(ltObj.url);
-    ltObj.campaign = $('#campaign-param-entry').val().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\s+/g, '_');
-    //$('#campaign-param-entry').validateField();
-    ltObj.content = $('#content-param-entry').val().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\s+/g, '_');
-    ltObj.term = $('#term-param-entry').val().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\s+/g, '_');
-
-//Generate links
-    ltObj.d_attribute.forEach(function(element) {
-
-        thisLink = ltObj.url + '?';
-        if (element.d_source == 'Internal') {
-            if (ltObj.campaign) thisLink += 'itm_campaign=' + ltObj.campaign + '&';
-            thisLink += 'itm_source=' + element.d_source + '&';
-            thisLink += 'itm_medium=' + element.d_medium;
-
-            if (ltObj.content) thisLink += '&itm_content=' + ltObj.content;
-            if (ltObj.term) thisLink += '&itm_term=' + ltObj.term;
+        let thisLink = ltObj.url + '?';
+        if ( ltObj.source == 'oshpd.ca.gov' ) {
+            thisLink += 'itm_campaign=' + ltObj.campaign;
+            thisLink += '&itm_source=' + ltObj.source;
+            thisLink += '&itm_medium=' + ltObj.medium;
+            if ( ltObj.lcontent ) thisLink += '&itm_content=' + ltObj.lcontent;
+            if ( ltObj.term ) thisLink += '&itm_term=' + ltObj.term;
         } else {
-            if (ltObj.campaign) thisLink += 'utm_campaign=' + ltObj.campaign + '&';
-            thisLink += 'utm_source=' + element.d_source + '&';
-            thisLink += 'utm_medium=' + element.d_medium;
-
-            if (ltObj.content) thisLink += '&utm_content=' + ltObj.content;
-            if (ltObj.term) thisLink += '&utm_term=' + ltObj.term;
+            thisLink += 'utm_campaign=' + ltObj.campaign;
+            thisLink += '&utm_source=' + ltObj.source;
+            thisLink += '&utm_medium=' + ltObj.medium;
+            if ( ltObj.lcontent ) thisLink += '&utm_content=' + ltObj.lcontent;
+            if ( ltObj.term ) thisLink += '&utm_term=' + ltObj.term;
         }
-        console.log(element.d_source);
-        ltObj.htmlOutput.push(thisLink);
+        ltObj.trackingLink = thisLink;
+
+        // link and copy button display
+        ltObj.htmlOutput =
+            '<p>' +
+            '<span class="URLdisplay d-block p-2">' +
+            ltObj.trackingLink +
+            '</span> ' + 
+            '<a href="#" onclick="return false;" class="copyURL btn btn-outline-dark mt-1" data-clipboard-text="' +
+            ltObj.trackingLink +
+            '"> Copy </a>' +
+            '</p>';
+
+        $('#generated-url').html(ltObj.htmlOutput);
     });
 
-//URL HTML generator 
-    ltObj.htmlOutput.forEach(function(link) {
-        ltObj.htmlCopy += '<p>'+
-            '<a href="#" class="copyURL btn btn-default" data-clipboard-text="' +
-            link +
-            '"> Copy </a> ' +
-            '<span class="URLdisplay">' +
-            link +
-            '</span></p>\n';
+    function checkProtocol(iURL) {
+        //force HTTPS compliance
+        var pURL;
+        if (!(iURL.search(/[^:\/\/]*$/) == 0)) {
+            pURL = iURL.match(/[^:\/\/]*$/);
+            pURL = 'https://' + pURL;
+
+        } else if (!(iURL.search(/[^:\/\/]*$/) == 1)) {
+            pURL = iURL;
+            pURL = 'https://' + pURL;
+        }
+        return pURL;
+    }
+
+    function validChars( inputElement ) {
+        return inputElement.val().replace( /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '' ).replace( /\s+/g, '_' );
+    }
+
+    $( '#generated-url' ).html( ltObj.htmlCopy );
+
+    // add URL copier event lister and handler
+    clipboard = new ClipboardJS('#generated-url .copyURL');
+    clipboard.on('success', function(e) {
+        $(e.trigger).html('Copied');
+        $(e.trigger).prev().css('background-color', 'lightgreen');
+        return false;
     });
-    $('#gen-result').html(ltObj.htmlCopy);
-}
 
-$('#gen-result').html(ltObj.htmlCopy);
-
-// add URL copier event lister and handler
-clipboard = new ClipboardJS('#gen-result .copyURL');
-clipboard.on('success', function(e) {
-    $('#gen-result .copyURL').html(' Copy ');
-    $('#gen-result .copyURL').next().css('background', 'none');
-    $(e.trigger).html('Copied');
-    $(e.trigger).next().css('background-color', 'lightgreen');
-});
-
-$("#link-param").find("input[type=checkbox]").click(function() {
-    GenerateLinks();
-})
+})( jQuery );
